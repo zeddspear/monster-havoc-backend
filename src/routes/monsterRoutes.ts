fb6c4a4b26f85750575909f8996859ca2e7a9be5
Shoaib <xshoaibahmedx007@gmail.com>
2024-08-25 01:47:20 +0500

    Monster Api fix

--- src/routes/monsterRoutes.ts
+++ src/routes/monsterRoutes.ts
@@ -8,7 +8,7 @@ import protect from "../middleware/authMiddleware";
 import checkAdmin from "../middleware/checkAdmin";
 const router = express.Router();
 
-router.get("/getAllMonsters", protect, getAllMonsters);
+router.get("/getAllMonsters", getAllMonsters);
 router.get("/getUserMonsters", protect, getUserMonsters);
 router.post("/addMonster", protect, checkAdmin, addMonster);
 export default router;
